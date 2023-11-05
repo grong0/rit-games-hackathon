9a3f931beac60f1d604f1be46fd4dcdadf14c340
Garrett59100 <Garrett59100@gmail.com>
2023-11-04 21:38:36 -0400

    Finished Review and Expanded CSS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,26 @@ function Location(props) {
                 <h3 className="location-address">props.address</h3>
             </div>
             <div className="location-rating-wrapper">
-                <div className="location-rating">props.rating</div>
-                <div className="location-incidents">props.incidents</div>
+                <h2 className="location-rating">props.rating</h2>
+                <h3 className="location-incidents">props.incidents</h3>
             </div>
         </div>
     )
 }
 
-function Review() {
+function Review(props) {
     return (
         <div className="review">
+            {/* <h2 className="review-name">props.name</h2>
+            <h2 className="review-score">props.score</h2>
+            <p className="review-content">props.content</p>  */}
 
+            <div className="review-title-wrapper">
+                <h2 className="review-name">props.name</h2>
+                <h3 className="review-author">props.author</h3>
+            </div>
+            <div className="review-score">props.score</div>
+            <div className="review-content">props.content</div>
         </div>
     )
 }
@@ -26,7 +35,15 @@ function Review() {
 function Home() {
 	return (
 		<div id="wrapper">
-            <Location name="Burger King" address="420 Gamer Blvd" rating="4.2" incidents=""/>
+            <div id="sidebar-wrapper">
+                <div id="sidebar-locations">
+                    <Location name="Burger King" address="420 Gamer Blvd" rating="4.2" incidents=""/>
+                </div>
+                <div id="sidebar-reviews">
+                    <Review name="I hate burgerking" score={5.2} content="I don't like their chicken nuggets. Nuff said."/>
+                </div>
+            </div>
+            
             <div id="map"></div>
 		</div>
 	);
